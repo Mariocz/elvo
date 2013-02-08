506a253a3cdcf92b3608b429e077fcf35160e013
Milos Divis <milosdivis@gmail.com>
2013-02-08 15:14:49 +0100

    login admin:login frontend complete

--- app/presenters/AdminPresenter.php
+++ app/presenters/AdminPresenter.php
@@ -5,18 +5,22 @@ class AdminPresenter extends BasePresenter {
     /** @var Nette\Database\Connection */
     private $db;
 
-    protected function startup()
-    {
+    protected function startup() {
         parent::startup();
-        
+
         // vytvoří instanci služby a uloží do vlastnosti presenteru
         $this->db = $this->context->database;
     }
-    
+
     public function renderDefault() {
         if (!$this->user->isLoggedIn()) {
             $this->redirect('Admin:login');
         }
+    }
+
+    protected function createComponentLogin() {
+
+        return new LoginControl($this->user);
     }
 
     public function actionLogout() {
