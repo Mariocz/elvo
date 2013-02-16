fb5c0da9b4252fe20fc49e65cbcb784d92c09c3a
Milos Divis <milosdivis@gmail.com>
2013-02-16 14:57:08 +0100

    Topeni page added, footer css alterd

---
+++ app/presenters/TopeniPresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Homepage presenter.
+ */
+class TopeniPresenter extends BasePresenter {
+
+    protected function startup()
+    {
+        parent::startup();
+    }
+
+    public function renderDefault() {
+    }
+}
